53390fe0b2691ad3385e364c95de8d557cf656c1
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-05-03 09:32:36 +0530

    n=int(input()) ##d=[ ] ##for i in range(n): ##    val=int(input()) ##    d.append(val) d=list(map(int,input().split())) os=0 for i in range(0,n):     s=d[i]     s=int(s)     if s%2!=0:         os+=s print(os)

---
+++ ODD_Sum.py
@@ -0,0 +1,13 @@
+n=int(input())
+##d=[ ]
+##for i in range(n):
+##    val=int(input())
+##    d.append(val)
+d=list(map(int,input().split()))
+os=0
+for i in range(0,n):
+    s=d[i]
+    s=int(s)
+    if s%2!=0:
+        os+=s
+print(os)
