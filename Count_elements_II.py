c2593afa8b0cc25bb1de6b252d3a31d6607e8de7
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-05-14 20:11:54 +0530

    n,m=map(int,input().split()) d1=list(map(int,input().split())) d2=list(map(int,input().split())) #print(d1) #print(d2) d1=set(d1) d1=list(d1) d2=set(d2) d2=list(d2) l1=len(d1) l2=len(d2) c=0 for i in range(0,l1):     s=d1[i]     if s not in d2:         c+=1 for j in range(0,l2):     s=d2[j]     if s not in d1:         c+=1 print(c)

---
+++ Count_elements_II.py
@@ -0,0 +1,21 @@
+n,m=map(int,input().split())
+d1=list(map(int,input().split()))
+d2=list(map(int,input().split()))
+#print(d1)
+#print(d2)
+d1=set(d1)
+d1=list(d1)
+d2=set(d2)
+d2=list(d2)
+l1=len(d1)
+l2=len(d2)
+c=0
+for i in range(0,l1):
+    s=d1[i]
+    if s not in d2:
+        c+=1
+for j in range(0,l2):
+    s=d2[j]
+    if s not in d1:
+        c+=1
+print(c)
