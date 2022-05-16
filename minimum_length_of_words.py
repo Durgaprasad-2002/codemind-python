84240b9085dc8a4786d853b98e28a641cd4958f8
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-05-16 22:18:40 +0530

    n=input() #print(n) l=len(n) c1=0 c=[] for i in range(l):     c1+=1     if i==l-1:         c+=[c1]         break     elif n[i]==' ':         c+=[c1-1]         c1=0 print(min(c))

---
+++ minimum_length_of_words.py
@@ -0,0 +1,14 @@
+n=input()
+#print(n)
+l=len(n)
+c1=0
+c=[]
+for i in range(l):
+    c1+=1
+    if i==l-1:
+        c+=[c1]
+        break
+    elif n[i]==' ':
+        c+=[c1-1]
+        c1=0
+print(min(c))
