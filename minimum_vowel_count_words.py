c970b4f7575f1a11f3272c50331ac51eafe61ca2
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-05-17 19:37:59 +0530

    n=input() s1="aeiouAEIOU" d=list(n) c=0 c1=[] h=0 for i in d:     if i in s1:         c+=1     if i is ' ':         h=1         c1+=[c]         c=0 if h==1:     c1+=[c]     m=min(c1)     mn=c1.count(m)     print(mn) else:     print(1)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,20 @@
+n=input()
+s1="aeiouAEIOU"
+d=list(n)
+c=0
+c1=[]
+h=0
+for i in d:
+    if i in s1:
+        c+=1
+    if i is ' ':
+        h=1
+        c1+=[c]
+        c=0
+if h==1:
+    c1+=[c]
+    m=min(c1)
+    mn=c1.count(m)
+    print(mn)
+else:
+    print(1)
