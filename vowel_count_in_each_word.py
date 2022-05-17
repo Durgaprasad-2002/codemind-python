6cbde9800572368ecc49feff98b947c4101ff8ed
Durgaprasad-2002 <84697619+Durgaprasad-2002@users.noreply.github.com>
2022-05-17 19:34:04 +0530

    n=input() s1="aeiouAEIOU" d=list(n) c=0 c1=[] h=0 for i in d:     if i in s1:         c+=1     if i is ' ':         h=1         c1+=[c]         c=0 if h==1:     c1+=[c]     print(*c1) else:     print(c)

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,18 @@
+n=input()
+s1="aeiouAEIOU"
+d=list(n)
+c=0
+c1=[]
+h=0
+for i in d:
+    if i in s1:
+        c+=1
+    if i is ' ':
+        h=1
+        c1+=[c]
+        c=0
+if h==1:
+    c1+=[c]
+    print(*c1)
+else:
+    print(c)
